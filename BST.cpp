ca4c0b54ebd9951557c681737ed3f4d39a4f9f9d
Shubham Prasad <subho@Subhams-MacBook-Pro.local>
2021-10-22 14:26:58 +0530

    adding some more data structures code

---
+++ BST.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+// This is the implementation of the Binary Search Tree
+
+struct Node {
+	int data;
+	struct Node *left;
+	struct Node *right;
+	Node(int val) {
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+// Method to create a node in BST.
+void insertNode(int val, Node* &root) {
+	if (root == NULL) {
+		root = new Node(val);
+		return;
+	}
+	if (val > root->data) {
+		insertNode(val, root->right);
+	}
+	else insertNode(val, root->left);
+}
+
+// Method for printing the node in inorder traversal.
+void printBST(Node* &root) {
+	if (root == NULL) return;
+	printBST(root->left);
+	cout << root->data << " ";
+	printBST(root->right);
+}
+
+// Method for forming a Binary Search Tree using a sorted array.
+Node* ArrayToBST(vector<int> a) {
+	if (a.size() == 0) return NULL;
+	if (a.size() == 1) return new Node(a[0]);
+	int mid = a.size() / 2;
+	Node* root = new Node(a[mid]);
+	root->left = ArrayToBST({a.begin(), a.begin() + mid});
+	root->right = ArrayToBST({a.begin() + mid + 1, a.end()});
+	return root;
+}
+
+// Find the Kth Smallest Element in a BST
+
+Node* findKthSmallest(Node* root, int &k) {
+	if (root == NULL) return NULL;
+	Node* left = findKthSmallest(root->left, k);
+	if (left != NULL) return left;
+	k -= 1;
+	if (k == 0) return root;
+	return findKthSmallest(root->right, k);
+}
+
+
+	
+
+int main() {
+	vector<int> a{-10,-3,0,5,9};
+	Node *root = ArrayToBST(a);
+	printBST(root);
+}
---
+++ heap.cpp
@@ -0,0 +1,187 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif 
+
+
+// this is the implementation of binary heap
+// this is the implementation of the binary min heap
+
+
+// Min Heap class
+void swap(int *x, int *y){
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+class MinHeap {
+	public: 
+	int *arr;
+	int size;
+	int capacity;
+	
+	// contructor for initialisatoin 
+	MinHeap(int c) {
+		arr = new int[c];
+		size = 0;
+		capacity = c;
+	}
+	
+	// find the left element of root "i"
+	int left(int i) {
+		return (2*i + 1);
+	}
+	
+	// find the right element of root "i"
+	int right(int i) {
+		return (2*i + 2);
+	}
+	
+	// find the parent of root "i"
+	int parent(int i) {
+		return (i - 1)/2;
+	}
+	
+	// method to implement insert operation in binary MinHeap
+	bool insert(int val) {
+		if (size == capacity) {
+			return false;
+		}
+		size += 1;
+		// putting element at the last index of the array
+		arr[size - 1] = val;
+		for (int i = size - 1; i != 0 && arr[parent(i)] > arr[i];) {
+			swap(arr[i], arr[parent(i)]);
+			i = parent(i);
+		}
+		return true;
+	}
+	
+	// Heapify Operation in Binary Min Heap
+	// worst case time complexity of heapify is O(H) where H is the height of the binary tree
+	// Space complextity is O(H).
+	void heapify(int i) {
+		int l = left(i);
+		int r = right(i);
+		int smallest = i;
+		if (l < size && arr[l] < arr[smallest]) smallest = l;
+		if (r < size && arr[r] < arr[smallest]) smallest = r;
+		if (smallest != i) {
+			swap(&arr[i], &arr[smallest]);
+			heapify(smallest);
+		}
+	}
+	
+	// Method to find the minimum value of Min Heap. 
+	// Time Complexity is O(1)
+	int getMin() {
+		return arr[0];
+	}
+	
+	// Method to find the minimum element from the Min Heap and removes the minimumm element as well
+	
+	int extractMin() {
+		if (size == 0) return INT_MAX;
+		if (size == 1) {
+			size -= 1;
+			return arr[0];
+		}
+		swap(&arr[0], &arr[size-1]);
+		size -= 1;
+		heapify(0);
+		return arr[size];
+	}
+	
+	// Method to print the Heap
+	void printHeap() {
+		for (int i = 0; i < size; i++) {
+			cout << arr[i] << " ";
+		}
+	}
+	
+	// Method for updating/decreasing a value in binary Min Heap
+	
+	void update(int i, int val) {
+		arr[i] = val;
+		while (i != 0 && arr[parent(i)] > arr[i]) {
+			swap(arr[parent(i)], arr[i]);
+			i = parent(i);
+		}
+	}
+	
+	// Method to delete a particular value in Min Heap
+	
+	void deleteNode(int i) {
+		update(i, INT_MIN);
+		extractMin();
+	}
+	
+	// Method for building a Min Heap from a random array
+	void buildMinHeap() {
+		for (int i = (size - 2) / 2; i >= 0; i--) {
+			heapify(i);
+		}
+	}
+	
+	// Program to covert a min heap to max heap
+	
+	void maxHeapify(vector<int> &a, int i) {
+		int largest = i;
+		int l = 2 * i + 1;
+		int r = 2 * i + 2;
+		if (l < a.size() && a[l] > a[largest]) largest = l;
+		if (r < a.size() && a[r] > a[largest]) largest = r;
+		if (largest != i) {
+			swap(a[i], a[largest]);
+			maxHeapify(a, largest);
+		}
+	}
+	
+	void convert(vector<int> &a) {
+		for (int i = a.size() - 1; i >= 0; i--) {
+			maxHeapify(a, i);
+		}
+	}
+};
+
+int main() {
+	//~ MinHeap heap(10);
+	//~ for (int i = 10; i >= 1; i--) {
+		//~ heap.insert(i);
+		//~ heap.heapify(10 - i);
+	//~ }
+	//~ heap.printHeap();
+	//~ cout << heap.extractMin() << endl;
+	
+	//~ cout << heap.extractMin() << endl;
+	vector<int> a{3, 5, 9, 6, 8, 20, 10, 12, 18, 9};
+	MinHeap heap(10);
+	heap.convert(a);
+	debug(a);
+	return 0;
+}
